eba0561094f4d4aca4a14db3398ca63071f6a3b5
Suman Parajuli <sumansupercell@gmail.com>
2024-08-03 21:01:54 +0545

    changes in navicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import useAlert from '../hooks/useAlert'
+import NavLogo from "../assets/img/navlogo-next.png";
 
 const Navbar = () => {
     const navigate = useNavigate()
     const location = useLocation()
 
     const { setAlert, setShow } = useAlert();
-    const [navState, setNavState] = useState('shadow-5-strong')
+    // const [navState, setNavState] = useState('shadow-5-strong')
     window.addEventListener('scroll', () => {
-        let scroll = window.scrollY > 50
-        if (scroll) {
-            setNavState('bg-light')
-        } else {
-            setNavState('shadow-5-strong')
-        }
+        // let scroll = window.scrollY > 50
+        // if (scroll) {
+        //     setNavState('bg-light')
+        // } else {
+        //     setNavState('shadow-5-strong')
+        // }
 
     })
 
@@ -33,29 +34,31 @@ const Navbar = () => {
 
 
         <>
-            <nav className={`navbar navbar-expand-lg fixed-top ${navState}`}>
+            <nav className={`navbar navbar-expand-lg`}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Travel Log</Link>
+                    <Link className="navbar-brand" to="/">
+                        <img src={NavLogo} alt="navlogo" width={110} height={35} />
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
+                            <li className="nav-item mx-2">
                                 <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} aria-current="page" to="/">Home</Link>
                             </li>
                             {
                                 localStorage.getItem('auth-token') &&
-                                <li className="nav-item">
+                                <li className="nav-item mx-2">
                                     <Link className={`nav-link ${location.pathname === '/createpost' ? 'active' : ''}`} to="/createpost">Create Post</Link>
                                 </li>
                             }
 
-                            <li className="nav-item">
+                            <li className="nav-item mx-2">
                                 <Link className={`nav-link ${location.pathname === '/posts' ? 'active' : ''}`} to="/posts">Posts</Link>
                             </li>
 
-                            <li className="nav-item">
+                            <li className="nav-item mx-2">
                                 <Link className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`} to="/about">About</Link>
                             </li>
 
@@ -75,6 +78,7 @@ const Navbar = () => {
 
                         {/* <Link className='btn btn-primary mx-1' onClick={handleLogout}>Log Out</Link> */}
                     </div>
+                <hr style={{ color: "red", height: "20px", zIndex: 100 }} />
                 </div>
             </nav>
         </>
